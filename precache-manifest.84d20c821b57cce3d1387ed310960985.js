9a616855b98082ca1339211b2d93e21dfb4f892a
Mohamud Olow <khalif18@live.com>
2019-08-24 06:48:12 -0700

    Updates

---
+++ precache-manifest.84d20c821b57cce3d1387ed310960985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440f7f21486a8fc52e4d973f38a42e1e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "e69c40cabcf1b3bfc452",
+    "url": "/pomodoro-clock/static/css/main.3a2f1ce7.chunk.css"
+  },
+  {
+    "revision": "50b11c50570b05bc8c24",
+    "url": "/pomodoro-clock/static/js/2.085957c8.chunk.js"
+  },
+  {
+    "revision": "e69c40cabcf1b3bfc452",
+    "url": "/pomodoro-clock/static/js/main.16f975df.chunk.js"
+  },
+  {
+    "revision": "5e89a7485758138b8801",
+    "url": "/pomodoro-clock/static/js/runtime~main.00109e4b.js"
+  }
+]);
