f20d2c158713d85a3fc274bf8183b59b90ee4703
Mohamud Olow <khalif18@live.com>
2019-08-24 23:50:51 -0700

    add SetTimer component with props from the App component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,36 @@ import './App.css';
 
 const Header = () => <h1>Pomodoro Clock</h1>;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const SetTimer = ({type, value}) => (
+  <div>
+  <h2 id={`${type}-label`}>{(`${type}` === 'session') ? 'Session ' : 'Break '}Length</h2>
+<div id="setTimers-controls">
+<button id={`${type}-decrement`}>&darr;</button>&nbsp;
+<div id={`${type}-length`}>{value}</div>&nbsp;
+<button id={`${type}-increment`}>&uarr;</buttoqn>
+</div>
+</div>);
 
-  render() {
-    return (
-      <div className="App">
-      <Header />
-      </div>
-    );
-  }
+class App extends React.Component {
+constructor(props) {
+super(props);
+this.state = {
+breakValue: 5,
+sessionValue: 25
+}
+}
+
+render() {
+return (
+<div className="App">
+<Header />
+<div id="setting">
+<SetTimer type="break" value={this.state.breakValue} />
+<SetTimer type="session" value={this.state.sessionValue} />
+</div>
+</div>
+);
+}
 }
 
 export default App;
